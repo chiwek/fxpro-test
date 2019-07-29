ffbd1567ed326ddbec50b2c09f3b95cbbbcf0857
Ivan Stanisavljevic <ivan@answeryes.com>
2019-07-29 21:05:13 +0200

    products

--- angular/src/app/utils/guards/check-role-permission.guard.ts
+++ angular/src/app/utils/guards/check-role-permission.guard.ts
@@ -3,44 +3,43 @@ import {Injectable} from '@angular/core';
 
 import {ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router/src/router_state';
 import {UserService} from "../../services/user/user.service";
-import {ToastService} from "../../services/frontend/toast.service";
 import {environment} from "../../../environments/environment";
+import {ModalService} from "../../services/frontend/modal.service";
 
 @Injectable()
 export class CheckRolePermissionGuard implements CanActivate {
 
-    constructor(private user: UserService, private toast: ToastService) {
+    constructor(private user: UserService, private modal: ModalService) {
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
-        return true;
 
-        /*
         const redirectUrl = route['_routerState']['url'];
 
-        if (this.user.loggedIn) {
-            const role = this.user.userRole;
-            let trimmedUrl = redirectUrl.replace(environment.baseUrl + '/', '');
-
-            const idx = trimmedUrl.indexOf('?');
-            if (idx > -1) {
-                trimmedUrl = trimmedUrl.substring(0, idx);
-            }
-
-            if (trimmedUrl.substring(0, 1) === '/') {
-                trimmedUrl = trimmedUrl.substring(1);
-            }
-
-            if (role.canOpenUrl(trimmedUrl)) {
-                return true;
-            }
-
+        if (!this.user.loggedIn) {
+            return false;
         }
 
-        this.toast.show('You do not have permission for this action.', 'error');
+        const role = this.user.userRole;
+        let trimmedUrl = redirectUrl.replace(environment.baseUrl + '/', '');
+
+        const idx = trimmedUrl.indexOf('?');
+        if (idx > -1) {
+            trimmedUrl = trimmedUrl.substring(0, idx);
+        }
+
+        if (trimmedUrl.substring(0, 1) === '/') {
+            trimmedUrl = trimmedUrl.substring(1);
+        }
+
+        if (role.canOpenUrl(trimmedUrl)) {
+            return true;
+        }
+
+        this.modal.info('You do not have permission for this action.');
+
 
         return false;
-        */
     }
 }
--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -25,5 +25,165 @@ class PermissionSeeder extends Seeder
             'permission_id' => 1,
         ]);
 
+        Permission::create([
+            'name' => 'Users list',
+            'url' => 'staff/list',
+            'action' => 'staffListUrl'
+        ]);
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 2,
+        ]);
+
+        Permission::create([
+            'name' => 'Users view',
+            'url' => 'staff/view',
+            'action' => 'staffViewUrl'
+        ]);
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 3,
+        ]);
+
+        Permission::create([
+            'name' => 'Users form',
+            'url' => 'staff/form',
+            'action' => 'staffFormUrl'
+        ]);
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 4,
+        ]);
+
+
+
+        Permission::create([
+        'name' => 'Main Menu Clients',
+        'url' => 'clients',
+        'action' => 'clientsUrl'
+    ]);
+
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 5,
+        ]);
+        RolePermission::create([
+            'role_id' => 2,
+            'permission_id' => 5,
+        ]);
+
+        Permission::create([
+            'name' => 'Clients list',
+            'url' => 'clients/list',
+            'action' => 'clientsListUrl'
+        ]);
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 6,
+        ]);
+
+        RolePermission::create([
+            'role_id' => 2,
+            'permission_id' => 6,
+        ]);
+
+        Permission::create([
+            'name' => 'Clients view',
+            'url' => 'clients/view',
+            'action' => 'clientsViewUrl'
+        ]);
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 7,
+        ]);
+
+        RolePermission::create([
+            'role_id' => 2,
+            'permission_id' => 7,
+        ]);
+
+        Permission::create([
+            'name' => 'Clients form',
+            'url' => 'clients/form',
+            'action' => 'clientsFormUrl'
+        ]);
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 8,
+        ]);
+        RolePermission::create([
+            'role_id' => 2,
+            'permission_id' => 8,
+        ]);
+
+        Permission::create([
+            'name' => 'Main Menu Products',
+            'url' => 'products',
+            'action' => 'productsUrl'
+        ]);
+
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 9,
+        ]);
+        RolePermission::create([
+            'role_id' => 2,
+            'permission_id' => 9,
+        ]);
+
+        Permission::create([
+            'name' => 'Products list',
+            'url' => 'products/list',
+            'action' => 'productsListUrl'
+        ]);
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 10,
+        ]);
+
+        RolePermission::create([
+            'role_id' => 2,
+            'permission_id' => 10,
+        ]);
+
+        Permission::create([
+            'name' => 'Products view',
+            'url' => 'products/view',
+            'action' => 'productsViewUrl'
+        ]);
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 11,
+        ]);
+
+        RolePermission::create([
+            'role_id' => 2,
+            'permission_id' => 11,
+        ]);
+
+        Permission::create([
+            'name' => 'Products form',
+            'url' => 'products/form',
+            'action' => 'productsFormUrl'
+        ]);
+
+        RolePermission::create([
+            'role_id' => 1,
+            'permission_id' => 12,
+        ]);
+        RolePermission::create([
+            'role_id' => 2,
+            'permission_id' => 12,
+        ]);
     }
 }
