f1e4e45a70e8ecbd2858859c18da627ef13b4fd8
Ivan Stanisavljevic <ivan@answeryes.com>
2019-07-29 12:54:28 +0200

    seeds

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,6 +19,7 @@ class UserSeeder extends Seeder
             'email' => 'chiwek@gmail.com',
             'username' => 'chiwek',
             'password' => bcrypt('chiwek'),
+            'remember_token' => '',
             'role_id' => 1,
             'date_registered' => date('Y-m-d H:i:s'),
             'is_active' => true
@@ -30,6 +31,7 @@ class UserSeeder extends Seeder
             'email' => 'nenads978@gmail.com',
             'username' => 'nenad',
             'password' => bcrypt('nenad'),
+            'remember_token' => '',
             'role_id' => 2,
             'franchise_id' => 1,
             'date_registered' => date('Y-m-d H:i:s'),
